9a24d81e9e21d51eceac45024a03d6741e066457
Cyan <68551684+CyanChanges@users.noreply.github.com>
2023-07-26 15:41:13 +0800

    Update main.py

--- miaoscript-check/main.py
+++ miaoscript-check/main.py
@@ -6,18 +6,15 @@ headers = {"content-type": "application/json"}
 
 for _ in range(10):
 	r = requests.get("https://registry.npmjs.org/koishi-plugin-miaoscript")
-  	try:
-  		r.raise_for_status()
+	try:
+		r.raise_for_status()
 	except:
-  		print(r.text)
-
+		print(r.text)
 	result = r.json()
 	try:
-    		latest = result['dist-tags']['latest']
+		latest = result['dist-tags']['latest']
 		assert latest=='0.0.1', f"Version mismatch {latest}"
 	except Exception as e:
 		print(e, r.text)
-
 	print("complete ----------")
-
 	time.sleep(60)
