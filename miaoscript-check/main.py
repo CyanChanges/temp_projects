a95e0cb24437b388c6ce5641ed678d8b641cb19f
Cyan <CyanChanges@outlook.com>
2023-07-30 15:11:04 +0000

    Update print fmt

--- miaoscript-check/main.py
+++ miaoscript-check/main.py
@@ -4,6 +4,8 @@ from typing import Sequence, Any
 
 from aiohttp import ClientSession, ClientResponseError
 from rich import print
+from rich.text import Text
+from rich.highlighter import NullHighlighter
 from rich.logging import RichHandler
 from rich.console import Console
 from rich.traceback import install
@@ -14,7 +16,7 @@ install(console=console)
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+	level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
 )
 
 logger = logging.getLogger("checker")
@@ -28,94 +30,114 @@ NPM_REGISTRY_ENDPOINT = 'https://registry.npmjs.org/'
 SEARCH_REGISTRY_ENDPOINT = 'https://registry.koishi.chat/'
 
 async def check_if_dep(session: ClientSession, package_name: str, plugins: Sequence[PluginInfo], targets: Sequence[PluginInfo]) -> bool:
-    async with session.get(f'/{package_name}') as r1:
-                try:
-                    r1.raise_for_status()
-                except ClientResponseError:
-                    return False
+	async with session.get(f'/{package_name}') as r1:
+				try:
+					r1.raise_for_status()
+				except ClientResponseError:
+					return False
 
-                package_info = await r1.json()
+				package_info = await r1.json()
 
-                latest_version = package_info['dist-tags']['latest']
+				latest_version = package_info['dist-tags']['latest']
 
-                latest_package_json = package_info['versions'][latest_version]
-                latest_peer_deps = latest_package_json.get('peerDependencies', {})
-                latest_deps = latest_package_json.get('dependencies', {})
+				latest_package_json = package_info['versions'][latest_version]
+				latest_peer_deps = latest_package_json.get('peerDependencies', {})
+				latest_deps = latest_package_json.get('dependencies', {})
 
-                depend_with_peer_deps = any(
-                    match['package']['name'] in latest_peer_deps.keys()
-                    for match in targets
-                )
+				depend_with_peer_deps = any(
+					match['package']['name'] in latest_peer_deps.keys()
+					for match in targets
+				)
 
-                depend_with_deps = any(
-                    match['package']['name'] in latest_deps.keys()
-                    for match in targets
-                )
+				depend_with_deps = any(
+					match['package']['name'] in latest_deps.keys()
+					for match in targets
+				)
 
-                if depend_with_deps or depend_with_peer_deps:
-                    return True
-                
-    return False
+				if depend_with_deps or depend_with_peer_deps:
+					return True
+				
+	return False
 
 
 
 async def scan_deps(package_names: Sequence[str], plugins: Sequence[PluginInfo], targets: Sequence[PluginInfo]) -> Sequence[PluginInfo]:
-    async with ClientSession(NPM_REGISTRY_ENDPOINT, headers=headers) as session:
-        filter_coro = map(lambda name: check_if_dep(session, name, plugins, targets), package_names)
+	async with ClientSession(NPM_REGISTRY_ENDPOINT, headers=headers) as session:
+		filter_coro = map(lambda name: check_if_dep(session, name, plugins, targets), package_names)
 
-        filterer = await asyncio.gather(*filter_coro)
+		filterer = await asyncio.gather(*filter_coro)
 
-        return tuple(filter(lambda idx_plugin: filterer[idx_plugin[0]], enumerate(plugins)))
+		return tuple(filter(lambda idx_plugin: filterer[idx_plugin[0]], enumerate(plugins)))
 
 async def scan_once():
-    result = None
+	result = None
 
-    async with ClientSession(SEARCH_REGISTRY_ENDPOINT, headers=headers) as ss:
-        async with ss.get('/', compress=True) as r:
-            try:
-                r.raise_for_status()
-            except ClientResponseError:
-                logger.warning(await r.text())
-                raise
+	async with ClientSession(SEARCH_REGISTRY_ENDPOINT, headers=headers) as ss:
+		async with ss.get('/', compress=True) as r:
+			try:
+				r.raise_for_status()
+			except ClientResponseError:
+				logger.warning(await r.text())
+				raise
 
-            result = await r.json()
+			result = await r.json()
 
-    assert result
+	assert result
 
-    plugins = result['objects']
-    short_names = tuple(map(lambda p: p['shortname'], plugins))
+	plugins = result['objects']
+	short_names = tuple(map(lambda p: p['shortname'], plugins))
 
-    matches_pub = []
+	matches_pub = []
 
-    for idx, name in enumerate(short_names):
-        if 'miao' in name or plugins[idx]['package']['publisher']['email'] == 'admin@yumc.pw':
-            matches_pub.append(plugins[idx])
-    
-    
-    matches_by_deps = await scan_deps([name for name in tuple(map(lambda p: p['package']['name'], plugins))], plugins, matches_pub)    
+	for idx, name in enumerate(short_names):
+		if 'miao' in name or plugins[idx]['package']['publisher']['email'] == 'admin@yumc.pw':
+			matches_pub.append(plugins[idx])
+	
+	
+	matches_by_deps = await scan_deps([name for name in tuple(map(lambda p: p['package']['name'], plugins))], plugins, matches_pub)    
 
-    return matches_pub, matches_by_deps if matches_by_deps else []
+	return matches_pub, matches_by_deps if matches_by_deps else []
 
 def print_package(pkg_info):
-    print(
-        f"{pkg_info['shortname']} | "
-        f"{pkg_info['package']['publisher']['username']} | "
-        f"{pkg_info['package']['version']}" 
-    )
+	pack = pkg_info['package']
+	links = pack['links']
+	publisher = pack['publisher']
+	pack_page = links.get('homepage', links.get('repository', links['npm']))
+	publisher_page = f"https://www.npmjs.org/~{publisher['username']}"
+	version_page = f"{links['npm']}/v/{pack['version']}"
+
+	# print(pack_page, publisher_page)
+
+	split_symbol = '[gray]|[/gray]'
+
+	renderables = (
+		Text(pkg_info['shortname'], f"yellow link {pack_page}"), split_symbol, 
+		Text(publisher['username'], f"blue link {publisher_page}"), split_symbol, 
+		Text(f"v{pkg_info['package']['version']}", f"cyan link {version_page}")
+	)
+
+	console.print(*renderables)
+	
 
 def main():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    
-    m_pub, m_by_dep = loop.run_until_complete(scan_once())
+	from time import perf_counter
+	
+	logger.info("[blue]Starting scan...", extra={"markup": True, "highlighter": NullHighlighter()})
+	
+	start_time = perf_counter()
 
-    [print_package(match) for match in m_pub]
-    [print_package(match) for match in m_by_dep]
+	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(loop)
+	
+	m_pub, m_by_dep = loop.run_until_complete(scan_once())
 
-    logger.debug(m_pub)
-    logger.debug(m_by_dep)
-    
-    logger.info("[green]complete", extra={"markup": True})
+	[print_package(match) for match in m_pub]
+	[print_package(match) for match in m_by_dep]
+
+	logger.debug(m_pub)
+	logger.debug(m_by_dep)
+	
+	logger.info(f"[green]Completed the scan in {perf_counter() - start_time:0.4f} s", extra={"markup": True})
 
 if __name__ == '__main__':
-     main()
+	 main()
